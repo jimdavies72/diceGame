297b477239f8492ef22002c110aef867dc15503f
James Davies <60436624+jimdavies72@users.noreply.github.com>
2021-12-02 09:13:17 +0000

    roll function from switch to array

--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
+const dieImageArray = [
+  "dice-roll.gif",
+  "die-one.png",
+  "die-two.png",
+  "die-three.png",
+  "die-four.png",
+  "die-five.png",
+  "die-six.png",
+];
+
+const soundEffectsArray = ["shakeDice.wav", "lostGame.wav", "wonGame.wav"];
+
 const btns = document.querySelectorAll(".btn");
-const splash = document.querySelector(".splash")
+const splash = document.querySelector(".splash");
 const onePlayer = document.querySelector(".one-player-board");
 const twoPlayer = document.querySelector(".two-player-board");
 const playerName = document.querySelector(".player-name");
 const scoreDisplay = document.querySelector(".score");
-const diceImg = document.querySelector(".dice-img")
-const gameStatus = document.getElementById("status")
-let currentscore = 0
-let gameType = ""
+const diceImg = document.querySelector(".dice-img");
+const gameStatus = document.getElementById("status");
+let currentScore = 0;
+let currentGameType = "";
 const defaultMediaPath = "./media/";
 const defaultDiceImg = `${defaultMediaPath}dice-roll.gif`;
 
 // random number generator
-  const getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,8 +34,9 @@ const defaultDiceImg = `${defaultMediaPath}dice-roll.gif`;
 const playAudio = (soundPath) => {
   const audio = new Audio(soundPath);
   audio.play();
-}
+};
 
+// make board we want visible and hide the ones we dont 
 const toggleVisible = (boardType) => {
   if (boardType === "splash") {
     splash.style.display = "";
@@ -46,104 +59,113 @@ const toggleVisible = (boardType) => {
   }
 };
 
-
-
 // starts or resets the game variables to init
 const startGame = (boardType) => {
   // what game board do we need? splash, 1 player or 2 player
   toggleVisible(boardType);
 
+  currentScore = 0;
   if (boardType === "Player 1") {
-  
-    currentscore = 0;
     scoreDisplay.textContent = `Score: 0`;
     playerName.innerHTML = boardType;
+    
     gameStatus.textContent = "Ready to roll!";
-    resetDiceImg();
-    let dieImgSrc = defaultDiceImg;
-    displayDiceImg(dieImgSrc);
+    initDiceImg();
+    
   } else if (boardType === "Player 2") {
     // 2 player
+    
     twoPlayerBoard();
-    resetDiceImg();
-    let dieImgSrc = defaultDiceImg;
-    displayDiceImg(dieImgSrc);
+    initDiceImg();
   }
 };
 
 // initiate the game
-startGame("splash")
-
+startGame("splash");
 
 // which button has been clicked
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     let btnClass = e.currentTarget;
-    
-    if (btnClass.classList.contains("player-btn")){
+
+    if (btnClass.classList.contains("player-btn")) {
       //player button clicked
-      gameType = btnClass.innerHTML;
-      startGame(gameType)
+      currentGameType = btnClass.innerHTML;
+      startGame(currentGameType);
     }
-     
-    // are we playing a player 1 game or a player 2 game
-    if (gameType = "Player 1"){
-      if (btnClass.classList.contains("roll-btn")) {
-        //roll dice button clicked
-        onePlayerRules(btnClass);
-        
-      } else if (btnClass.classList.contains("reset-btn")) {
+
+    if (btnClass.classList.contains("reset-btn")) {
         //reset the game board
         toggleRollNewGameBtn(btnClass, false);
 
         // reset game variables
-        startGame(gameType);
-      }
-    }else{
-      // Player 2 game
-
+        startGame(currentGameType);
+        
     }
     
+    // are we playing a player 1 game or a player 2 game
+    if ((currentGameType = "Player 1")) {
+      if (btnClass.classList.contains("roll-btn")) {
+        //roll dice button clicked
+        onePlayerRules(btnClass);
+      } 
+
+    } else {
+      // Player 2 game
+      if (btnClass.classList.contains("roll-btn")) {
+
+      } else if (btnClass.classList.contains("hold-btn")) {
+
+
+      }
+
+    }
   });
-})
+});
 
-
-const onePlayerRules = (btnClass) =>{
+const onePlayerRules = (btnClass) => {
   //roll dice button clicked
 
   let roll = rollDice();
-  currentscore += roll;
+  currentScore += roll;
   setTimeout(function () {
     let currentStatus = "";
     if (roll === 1) {
       // score of 1 = insta-lose
 
       currentStatus = "Lost! rolled a 1";
-      playAudio(`${defaultMediaPath}lostGame.wav`);
+      playAudio(`${defaultMediaPath}${soundEffectsArray[1]}`);
       toggleRollNewGameBtn(btnClass);
-    } else if (currentscore >= 21) {
+    } else if (currentScore >= 21) {
       // win!
 
-      scoreDisplay.textContent = `Score: ${currentscore}`;
+      scoreDisplay.textContent = `Score: ${currentScore}`;
       currentStatus = "wins!";
-      playAudio(`${defaultMediaPath}wonGame.wav`);
+      playAudio(`${defaultMediaPath}${soundEffectsArray[2]}`);
       toggleRollNewGameBtn(btnClass);
     } else {
       // we continue...
 
       currentStatus = "Playing...";
-      scoreDisplay.textContent = `Score: ${currentscore}`;
+      scoreDisplay.textContent = `Score: ${currentScore}`;
     }
     gameStatus.textContent = currentStatus;
   }, 3000);
-}
-
+};
 
 const twoPlayerBoard = () => {
+  let buttonsDisabled = ""
   twoPlayer.innerHTML = "";
-  for (let i = 0; i < 2; i++){
-    let html = `
-    <div class="container player${i + 1}">  
+  
+  for (let i = 0; i < 2; i++) {
+    //player 2 settings
+    if (i > 0){
+      buttonsDisabled = "disabled"
+    } else {
+      buttonsDisabled = "";
+    }
+        
+    let html = `<div class="container player${i + 1}">  
       <div class="player">
         <h3 class="player-name">Player ${i + 1}</h3>
       </div>
@@ -154,63 +176,35 @@ const twoPlayerBoard = () => {
         <h4>Current</h4>
         <h4 class="current-score">0</h4>
       </div>
-      <div class="dice-img"></div>  
-      <button class="btn roll-btn">Roll</button>
-      <button class="btn hold-btn">Hold</button>
-    <div>  
-  `;
+      <button class="btn ${buttonsDisabled} roll-btn">Roll</button>
+      <button class="btn ${buttonsDisabled} hold-btn">Hold</button>
+    <div>
+    <div class="dice dice-img"></div>`;
 
-  twoPlayer.innerHTML += html
+    twoPlayer.innerHTML += html;
   }
-
 };
 
-
-
-const rollDice = () =>{
-  let dieImg = ""
-  let score = 0
-  const soundPath = `${defaultMediaPath}shakeDice.wav`;
-
-  score = getRandomInt(1, 6);
-  switch (score) {
-    case 1:
-      dieImg = `${defaultMediaPath}die-one.png`;
-      break;
-    case 2:
-      dieImg = `${defaultMediaPath}die-two.png`;
-      break;
-    case 3:
-      dieImg = `${defaultMediaPath}die-three.png`;
-      break;
-    case 4:
-      dieImg = `${defaultMediaPath}die-four.png`;
-      break;
-    case 5:
-      dieImg = `${defaultMediaPath}die-five.png`;
-      break;
-    case 6:
-      dieImg = `${defaultMediaPath}die-six.png`;
-      break;
-  }
-  // remove the img element if it exists
-  resetDiceImg()
-  displayDiceImg(defaultDiceImg);
-
-  //play shaking dice sound
-  //playAudio(soundPath)
+const rollDice = () => {
+  let dieImg = "";
+  let score = 0;
   
-  const audio = new Audio(soundPath);
+  score = getRandomInt(1, 6);
+  
+  // remove the img element if it exists and replace with dice roll gif
+  resetDiceImg();
+   displayDiceImg(`${defaultMediaPath}${dieImageArray[0]}`);
+
+  const audio = new Audio(`${defaultMediaPath}${soundEffectsArray[0]}`);
   audio.play();
   audio.addEventListener("ended", () => {
     audio.currentTime = 0;
     resetDiceImg();
-    displayDiceImg(dieImg)
+    displayDiceImg(`${defaultMediaPath}${dieImageArray[score]}`);
   });
 
   return score;
-  
-}
+};
 
 const toggleRollNewGameBtn = (btn, isResetBtn = true) => {
   // during the game the button will be 'roll'
@@ -229,16 +223,24 @@ const toggleRollNewGameBtn = (btn, isResetBtn = true) => {
   }
 };
 
-const resetDiceImg = ()=> {
+
+const initDiceImg = () => {
+  
+  resetDiceImg();
+  displayDiceImg(defaultDiceImg);
+}
+
+const resetDiceImg = () => {
   //removes any existing images
   if (diceImg.hasChildNodes()) {
     diceImg.querySelectorAll("*").forEach((n) => n.remove());
   }
-}
+};
 
-const displayDiceImg = (src) =>{
+const displayDiceImg = (src) => {
   let img = document.createElement("img");
   img.src = src;
+  console.log(diceImg);
   diceImg.appendChild(img);
-}
-
+  
+};
